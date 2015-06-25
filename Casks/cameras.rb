25a349e320865f2eb50073f5b4e56c1e87d81a86
Sean Zhu <interestinglythere@gmail.com>
2015-06-25 13:27:20 -0700

    Add Cameras

---
+++ Casks/cameras.rb
@@ -0,0 +1,15 @@
+cask :v1 => 'cameras' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.flexibits.com/cameras/downloads/Cameras.dmg'
+  name 'Cameras'
+  homepage 'http://www.macupdate.com/app/mac/31606/cameras'
+  license :gratis
+
+  prefpane 'Cameras.prefPane'
+
+  caveats do
+    discontinued
+  end
+end
