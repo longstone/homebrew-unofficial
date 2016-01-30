909fe7fe41a5fd2bbeb8f01cb1ad04cc0be83bcb
Vítor Galvão <info@vitorgalvao.com>
2016-01-30 02:44:48 +0000

    updated batch-rip-actions-for-automator (1.1.1)

--- Casks/batch-rip-actions-for-automator.rb
+++ Casks/batch-rip-actions-for-automator.rb
@@ -3,6 +3,7 @@ cask 'batch-rip-actions-for-automator' do
   sha256 '223f443c1909c82e83574f8b155fe282ee360e492fa693a0d3c1e6d57a67e28d'
 
   url "https://dl.dropbox.com/u/242398/BatchRip/BatchRipActions-#{version}.dmg"
+  name 'Batch Rip Actions for Automator'
   homepage 'http://forums.macrumors.com/showthread.php?t=805573'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
