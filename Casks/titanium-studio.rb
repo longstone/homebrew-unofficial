526e0df8369dd836238603a24821cf3e51543d58
Vítor Galvão <info@vitorgalvao.com>
2016-01-30 03:03:31 +0000

    updated titanium-studio (latest)

--- Casks/titanium-studio.rb
+++ Casks/titanium-studio.rb
@@ -3,6 +3,7 @@ cask 'titanium-studio' do
   sha256 :no_check
 
   url 'https://titanium-studio.s3.amazonaws.com/latest/Titanium_Studio.dmg'
+  name 'Titanium Studio'
   homepage 'https://www.appcelerator.com/titanium/'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
