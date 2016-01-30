b8a6727a3a7cf15caa4d8868bc9ffd4afb3d91f4
Vítor Galvão <info@vitorgalvao.com>
2016-01-30 03:03:09 +0000

    updated stash (1.0)

--- Casks/stash.rb
+++ Casks/stash.rb
@@ -3,6 +3,7 @@ cask 'stash' do
   sha256 'd9f1e125e72ca61e3ba3fffc9b0ff920a9a1e4e48c223c555facf95686af2803'
 
   url "https://github.com/alehouse/Stash/raw/master/downloads/Stash_#{version}.tgz"
+  name 'Stash'
   homepage 'https://github.com/beelsebob/Stash'
   license :oss
 
