ed8a32c3a6c298f8e1f780a0314be8e2e1f459bf
Vítor Galvão <info@vitorgalvao.com>
2016-01-30 03:00:13 +0000

    updated limechat-noclose (2.42)

--- Casks/limechat-noclose.rb
+++ Casks/limechat-noclose.rb
@@ -3,6 +3,7 @@ cask 'limechat-noclose' do
   sha256 '804497d75f229ae87fdc7d6f050d1662c20b12f73c68e7750c37665ca93223fe'
 
   url "https://github.com/rolandwalker/limechat/releases/download/#{version}-hide-window-on-close/LimeChat.app.zip"
+  name 'LimeChat'
   homepage 'https://github.com/rolandwalker/limechat'
   license :oss
 
