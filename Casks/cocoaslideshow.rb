2c2061af65cc6c2b684936ad7db98faf908089c0
Vítor Galvão <info@vitorgalvao.com>
2016-01-30 02:46:08 +0000

    updated cocoaslideshow (0.6)

--- Casks/cocoaslideshow.rb
+++ Casks/cocoaslideshow.rb
@@ -3,6 +3,7 @@ cask 'cocoaslideshow' do
   sha256 'e930c05fc8cca61c72bb0821534d84438ba3f0e627f8d162a2a8e4392ef36331'
 
   url "https://github.com/alehouse/CocoaSlideShow/raw/master/downloads/CocoaSlideShow_#{version}.tgz"
+  name 'CocoaSlideShow'
   homepage 'https://github.com/nst/CocoaSlideShow'
   license :oss
 
