3b07990684078560bf5892d0049bddbcb72ffb6f
Vítor Galvão <info@vitorgalvao.com>
2016-01-30 03:04:38 +0000

    updated unused (1.1)

--- Casks/unused.rb
+++ Casks/unused.rb
@@ -3,6 +3,7 @@ cask 'unused' do
   sha256 '8b94c245babbac14198b07df81fdc393b4d7aa9f8a39ffecec79e6645d3f4243'
 
   url "https://github.com/alehouse/Unused/raw/master/downloads/Unused_#{version}.tgz"
+  name 'Unused'
   homepage 'https://github.com/jeffhodnett/Unused'
   license :oss
 
