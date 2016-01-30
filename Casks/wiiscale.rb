5cecb56ead8b51194f52f327de0209c7cacb2515
Vítor Galvão <info@vitorgalvao.com>
2016-01-30 03:05:07 +0000

    updated wiiscale (1.0)

--- Casks/wiiscale.rb
+++ Casks/wiiscale.rb
@@ -3,6 +3,7 @@ cask 'wiiscale' do
   sha256 '072ff61b9daf01cb209cd5e3c7b6eca676ca3a578d6cd3b48f804fd719399cfe'
 
   url "https://github.com/alehouse/wiiscale/raw/master/downloads/WiiScale_#{version}.tgz"
+  name 'WiiScale'
   homepage 'https://github.com/damariei/wiiscale'
   license :oss
 
