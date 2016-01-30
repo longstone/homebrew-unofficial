b17699f969fa2e7fd2122e9ffa9a5c28fe8392db
Vítor Galvão <info@vitorgalvao.com>
2016-01-30 02:46:48 +0000

    updated d3visualizer (1.0)

--- Casks/d3visualizer.rb
+++ Casks/d3visualizer.rb
@@ -3,6 +3,7 @@ cask 'd3visualizer' do
   sha256 '5f895f9822e2f5534755d7e4402ea794953720e82270e1006af2971ef5b9e98b'
 
   url "https://github.com/alehouse/D3Visualizer/raw/master/downloads/D3Visualizer_#{version}.tgz"
+  name 'D3Visualizer'
   homepage 'https://github.com/swisspol/D3Visualizer'
   license :oss
 
