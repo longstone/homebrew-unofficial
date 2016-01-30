63830fa191fff130c03248f67d811e7a77b5012f
Vítor Galvão <info@vitorgalvao.com>
2016-01-30 02:45:26 +0000

    updated bughub (1.0.1)

--- Casks/bughub.rb
+++ Casks/bughub.rb
@@ -3,6 +3,7 @@ cask 'bughub' do
   sha256 'd5fa398386b55ea6881be46e858c12dc906579d07d62fbc7bd5f4d03dd2fa1fd'
 
   url "https://github.com/alehouse/BugHub/raw/master/downloads/BugHub_#{version}.tgz"
+  name 'BugHub'
   homepage 'https://github.com/Me1000/BugHub'
   license :oss
 
